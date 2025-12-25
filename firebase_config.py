ca234c77ef20c3d3730a1c85e38d2d6258513c77
merajunnisajaved004-lgtm <merajunnisajaved004@gmail.com>
2025-12-25 22:18:24 +0500

    Add files via upload

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,11 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Load your Service Account Key
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+# Initialize Firebase using Streamlit Secrets
+if not firebase_admin._apps:
+    cred = credentials.Certificate(dict(st.secrets["FIREBASE"]))
+    firebase_admin.initialize_app(cred)
 
 # Create Firestore client
 db = firestore.client()
